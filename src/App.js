087fc1989f43077b10e19401de6e813eedad89b9
narogeby <nrg00007@mix.wvu.edu>
2024-02-25 14:54:21 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 002</h2>
+      <p>WVU ID: 800384058</p>
+      <p>Hi,  I am Nathanson Gageby</p>
+    </div>
+  );
+}
+
+export default App;
