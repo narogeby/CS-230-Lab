82d1dcdd420cec27daf4f1b5ec88b282b24a15cd
narogeby <nrg00007@mix.wvu.edu>
2024-03-03 14:00:27 -0500

    second part of the lab

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar.js';
+import Card from './Card.js';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>CS 230L</h1>
       <h2>Section - 002</h2>
       <p>WVU ID: 800384058</p>
       <p>Hi,  I am Nathanson Gageby</p>
+      <table>
+        <tr>
+          <td><Card /></td>
+          <td><Card /></td>
+          <td><Card /></td>
+        </tr>
+      </table>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,15 @@
+
+function Card() {
+
+    return (
+        <div class="card" style={{ width: 18 + 'rem' }}>
+            <div class="card-body">
+                <h5 class="card-title">Have you ever heard the tragedy of Darth Plegueis the Wise?</h5>
+                <p class="card-text">Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.</p>
+            </div>
+        </div>
+    );
+
+}
+
+export default Card
